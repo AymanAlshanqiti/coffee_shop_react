4cd34740290810d965dac5b7746759ec89aab82e
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-04-02 22:25:46 +0300

    import bootstrap to app.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import "./App.css";
 import Home from "./components/index";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
 class App extends Component {
   render() {
     return <Home className="App" />;
