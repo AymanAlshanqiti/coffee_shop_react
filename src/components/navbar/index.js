bd08b84d7b8a82f446a3d4be68d4d3dcaa24c083
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-04-07 23:43:51 +0300

    things

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -8,7 +8,6 @@ import { connect } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import {
-  faIdBadge,
   faIdCard,
   faSignInAlt,
   faUserPlus
