a462b5d6aac9a2d3dabc564f0b8d4731ade7e5fa
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-04-08 12:36:41 +0300

    add things

--- src/components/profiles/previousOrders.js
+++ src/components/profiles/previousOrders.js
@@ -7,7 +7,7 @@ import * as actionCreators from "../../store/actions/index";
 
 // Fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
 
 const formatAMPM = date => {
   let hours = date.getHours();
@@ -58,9 +58,6 @@ class PreviousOrders extends Component {
   }
 
   async componentDidUpdate(prevProps, prevState) {
-    console.log("TCL: Profile -> componentDidUpdate -> prevProps", prevProps);
-    console.log("TCL: Profile -> componentDidUpdate -> prevProps", this.props);
-
     if (prevProps.user !== this.props.user || !this.props.profile) {
       this.props.getProfileDetail();
 
@@ -108,32 +105,61 @@ class PreviousOrders extends Component {
             </div>
           </div>
         </div>
-
         <div className="col-8 mx-4 my-4 text-center">
           <h2 style={{ color: "#fe687b" }}>
-            <FontAwesomeIcon icon={faCoffee} style={{ color: "#fe687b" }} /> My
-            Previous orders
+            <FontAwesomeIcon
+              icon={faChevronCircleDown}
+              style={{ color: "#fe687b" }}
+            />{" "}
+            Order Detail
           </h2>
           <br />
-          <div className="row">
-            <div className="col-4 text-left">Product Name</div>
-            <div className="col-4 text-left">Quantity</div>
-            <div className="col-4">Sub Total</div>
-          </div>
 
-          <ul className="list-group">
-            {profile &&
-              customerOrders.map(ord => {
-                return (
-                  <li className="list-group-item d-flex justify-content-between align-items-center text-left">
-                    {" "}
-                    <span>1</span>
-                    <span>2</span>
-                    <span>3</span>
-                  </li>
-                );
-              })}
-          </ul>
+          <div className="row justify-content-md-center">
+            <div className="col-8">
+              <div className="col-12 mx-4 my-4 text-center">
+                {" "}
+                <table className="table table-hover text-left">
+                  <thead>
+                    <tr>
+                      <th scope="col" style={{ color: "#fe687b" }}>
+                        Order Id
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-center"
+                        style={{ color: "#fe687b" }}
+                      >
+                        Total Price
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-center"
+                        style={{ color: "#fe687b" }}
+                      >
+                        Created At
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-center"
+                        style={{ color: "#fe687b" }}
+                      >
+                        Pruducts Count
+                      </th>
+                      <th
+                        scope="col"
+                        className="text-center"
+                        style={{ color: "#fe687b" }}
+                      >
+                        Operations
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>{PreviousOrders}</tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
